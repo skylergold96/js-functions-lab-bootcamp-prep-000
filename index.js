9861315e31f646d72559a2446334e644ceeb13df
Skyler H Gold <skylergold96@gmail.com>
2019-11-22 21:15:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,3 @@ function happyHolidays(){
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo (holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-}
